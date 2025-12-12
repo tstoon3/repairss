2c61451039f6ccb39cd6a66481fcedaa838e6de8
tstoon3 <tstoon3@hotmail.com>
2025-12-12 14:41:12 +0700

    Update ready for Nuxtlink

---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Comform_ip from '~/component/comform_ip.vue'
+import Navbar from '~/component/navbar.vue'
+import Repairlist from '~/component/repairlist.vue'
+</script>
+
+<template>
+  <div>
+    <Head>
+      <title>หน้าแรก - ระบบแจ้งซ่อม</title>
+      <link rel="icon" href="/logo.ico">
+    </Head>
+
+    <Navbar />
+    <Comform_ip />
+    <Repairlist />
+  </div>
+</template>
